58d986bf61a8e4f5e4e401f9bd17081d933e4d22
Giovanni Ruiz <gioruiz@oscars-imac>
2018-01-18 16:37:23 -0800

    Initial Commit

---
+++ VoterReach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoterReach
+//
+//  Created by Giovanni Ruiz on 1/18/18.
+//  Copyright © 2018 VoterReach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
